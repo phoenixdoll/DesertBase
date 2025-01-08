232016832a7f5c225a51e8635de25df9ee59a0d1
phoenixdoll <phoenixdoll@gmail.com>
2025-01-08 13:18:02 +1100

    removed water from the river

--- Lots/Addica's Area - River Factory/Objects/spawnpoints.lua
+++ Lots/Addica's Area - River Factory/Objects/spawnpoints.lua
@@ -1,16 +1,22 @@
 function SpawnPoints()
 return {
+  constructionworker = {
+    { worldX = 2, worldY = 6, posX = 226, posY = 185, posZ = 0 }
+  },
+  fireofficer = {
+    { worldX = 2, worldY = 6, posX = 226, posY = 185, posZ = 0 }
+  },
+  parkranger = {
+    { worldX = 2, worldY = 6, posX = 226, posY = 185, posZ = 0 }
+  },
+  policeofficer = {
+    { worldX = 2, worldY = 6, posX = 226, posY = 185, posZ = 0 }
+  },
+  securityguard = {
+    { worldX = 2, worldY = 6, posX = 226, posY = 185, posZ = 0 }
+  },
   unemployed = {
-    { worldX = 0, worldY = 8, posX = 105, posY = 40, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 50, posY = 42, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 34, posY = 55, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 115, posY = 179, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 115, posY = 165, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 135, posY = 219, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 273, posY = 204, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 241, posY = 181, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 271, posY = 265, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 237, posY = 25, posZ = 0 }
+    { worldX = 2, worldY = 6, posX = 226, posY = 185, posZ = 0 }
   }
 }
 end
